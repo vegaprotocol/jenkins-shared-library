a4d449b76b97fd607be9e71c5612dc421a94803d
fkondej <fkondej@gmail.com>
2021-09-15 11:27:51 +0200

    Add slack helper methods

---
+++ vars/slack.groovy
@@ -0,0 +1,36 @@
+/* groovylint-disable DuplicateStringLiteral */
+
+String composeMessage(Map config) {
+        String name = config.get('name', 'Unknown CI')
+        String jobURL = config.get('jobURL', env.RUN_DISPLAY_URL)
+        String branch = config.get('branch', env.BRANCH_NAME)
+        String prURL = config.get('pr', env.CHANGE_URL)
+        String prID = config.get('prID', env.CHANGE_ID)
+        String message = " ${name} » <${jobURL}|Jenkins ${branch} Job>"
+        if (prURL != null) {
+            message += " » <${prURL}|GitHub PR #${prID}>"
+        }
+        return message
+}
+
+void slackSendCISuccess(Map config) {
+    String slackChannel = config.get('channel', '#tradingcore-notify')
+    String message = composeMessage(config)
+
+    slackSend(
+        channel: slackChannel,
+        color: 'good',
+        message: ":white_check_mark: ${message}",
+    )
+}
+
+void slackSendCIFailure(Map config) {
+    String slackChannel = config.get('channel', '#tradingcore-notify')
+    String message = composeMessage(config)
+
+    slackSend(
+        channel: slackChannel,
+        color: 'danger',
+        message: ":red_circle: ${message}",
+    )
+}
