0ec0cc496341a87877692f1718061f0dde6f486c
fkondej <fkondej@gmail.com>
2021-11-12 21:56:29 +0100

    Pass arguments from helper function to the pipeline

--- vars/devnetDeploy.groovy
+++ vars/devnetDeploy.groovy
@@ -4,13 +4,21 @@ import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 void call(Map config = [:]) {
     String devnetDeployJob = 'cd/Devnet deploy'
     Boolean ignoreFailure = config.ignoreFailure ? "${config.ignoreFailure}".toBoolean() : false
+    List buildParameters = [
+            string(name: 'VEGA_CORE_VERSION', value: config.vegaCore ?: pipelineDefaults.dev.vegaCoreVersion),
+            string(name: 'DEPLOY_CONFIG', value: config.deployConfig ?: pipelineDefaults.dev.deployConfig),
+            string(name: 'RESTART_NETWORK', value: config.restart ?: pipelineDefaults.dev.restartNetwork),
+            string(name: 'CREATE_MARKETS', value: config.createMarkets ?: pipelineDefaults.dev.createMarkets),
+            string(name: 'DEVOPS_INFRA_BRANCH', value: config.devopsInfra ?: pipelineDefaults.dev.devopsInfraBranch),
+        ]
 
     echo 'Deploy to Devnet network'
 
     RunWrapper deployJob = build(
         job: devnetDeployJob,
         propagate: false,  // don't fail yet
-        wait: true
+        wait: true,
+        parameters: buildParameters
     )
 
     echo "Deploy to Devnet pipeline: ${deployJob.absoluteUrl}"
