69e2d490ac5251c26467cbc555c2b75b27718f6d
fryd <fkondej@gmail.com>
2021-12-29 23:35:33 +0100

    Fail stage and pipeline if a container is down, but keep the pipeline running

--- vars/pipelineDockerisedVega.groovy
+++ vars/pipelineDockerisedVega.groovy
@@ -831,6 +831,16 @@ void runMainStages(Closure inputMainStage, DockerisedVega dockerisedVega, Map va
                     script: """#!/bin/bash -e
                     ${dockerLogsCommand} ${longName}
                     """
+                // catchError - ff the body throws an exception, mark the build 
+                // and stage as a failure, but nonetheless continue to execute the Pipeline
+                catchError(message: "${shortName} stopped") {
+                    // docker top - displays the running processes of a container
+                    // it fails if container is stopped or does not exist
+                    sh label: "Check if container is running ${shortName}",
+                        script: """#!/bin/bash -e
+                        docker top ${longName}
+                        """
+                }
             }
         }
     }
