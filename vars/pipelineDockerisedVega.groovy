a508b4e83dbc2d33dfd24be53ecb23b81124a5eb
fryd <fkondej@gmail.com>
2021-12-30 00:10:30 +0100

    Mark stage as failure

--- vars/pipelineDockerisedVega.groovy
+++ vars/pipelineDockerisedVega.groovy
@@ -833,7 +833,7 @@ void runMainStages(Closure inputMainStage, DockerisedVega dockerisedVega, Map va
                     """
                 // catchError - ff the body throws an exception, mark the build 
                 // and stage as a failure, but nonetheless continue to execute the Pipeline
-                catchError(message: "${shortName} stopped") {
+                catchError(message: "${shortName} stopped", stageResult: 'FAILURE') {
                     // docker top - displays the running processes of a container
                     // it fails if container is stopped or does not exist
                     sh label: "Check if container is running ${shortName}",
