7e8468783dbc438551f5689da564db5851e71584
fryd <fkondej@gmail.com>
2022-01-24 00:43:10 +0100

    Add withGHCLI helper

---
+++ vars/withGHCLI.groovy
@@ -0,0 +1,24 @@
+
+void call(Map config = [:], Closure body=null) {
+    String credentialsId = config.get('credentialsId', null)
+    if (credentialsId && body) {
+        try {
+            withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'TOKEN', usernameVariable:'USER')]) {
+                // Workaround for user input:
+                //  - global configuration: 'gh config set prompt disabled'
+                sh label: 'Log in to a Gihub with CI', script: '''
+                    echo ${TOKEN} | gh auth login --with-token -h github.com
+                '''
+            }
+            body()
+        } finally {
+            retry(3) {
+                script {
+                    sh label: 'Log out from Github', script: '''
+                        gh auth logout -h github.com
+                    '''
+                }
+            }
+        }
+    }
+}
