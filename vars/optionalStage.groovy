e8aac26facf56e53cbe4134ea72014b0f72541ff
fryd <fkondej@gmail.com>
2022-02-17 00:55:14 +0100

    Create optionalStage

---
+++ vars/optionalStage.groovy
@@ -0,0 +1,13 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
+void call(Map config=[:], Closure body=null) {
+    String stageName = config.name
+    Boolean when = config.when ? "${config.when}".toBoolean() : false
+    stage(stageName) {
+        if (when) {
+            body()
+        } else {
+            Utils.markStageSkippedForConditional(stageName)
+        }
+    }
+}
--- vars/skipStageIf.groovy
+++
@@ -1,9 +0,0 @@
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-
-void call(Boolean condition, Closure body=null) {
-    if (condition) {
-            Utils.markStageSkippedForConditional(STAGE_NAME)
-    } else {
-        body()
-    }
-}
