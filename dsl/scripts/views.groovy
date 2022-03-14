a812940be67c7327b76a727063e9051d712c2c1f
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-03-14 13:38:40 +0100

    add dsl stuff to automatically create jobsd

---
+++ dsl/scripts/views.groovy
@@ -0,0 +1,31 @@
+def createListView(Map args){
+    listView(args.name) {
+        recurse()
+        jobs {
+            args.jobs.each{ job ->
+                name(job)
+            }
+            if (args.regex){
+              regex(args.regex)
+            }
+        }
+        if (args.description){
+          description(args.description)
+        }
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+            favoriteColumn()
+        }
+    }
+}
+
+createListView(
+    regex: 'ansible.*'
+    description: "View with all ansible jobs"
+)
