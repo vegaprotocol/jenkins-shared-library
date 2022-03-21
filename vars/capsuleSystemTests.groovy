f532a497cd7f7666c918ea7fe230df8d4cb1f8a5
Daniel Hornik <daniel.1302@gmail.com>
2022-03-21 16:21:33 +0100

    Peer review changes

--- vars/capsuleSystemTests.groovy
+++ vars/capsuleSystemTests.groovy
@@ -1,20 +1,6 @@
 /* groovylint-disable 
   BuilderMethodWithSideEffects, CompileStatic, DuplicateStringLiteral, 
   FactoryMethodName, VariableTypeRequired */
-
-
-void gitClone(String repository, String branch, String directory, String credentialsId) {
-  dir(directory) {
-    checkout([
-      $class: 'GitSCM',
-      branches: [[name: branch]],
-      userRemoteConfigs: [[
-          url: 'git@github.com:vegaprotocol/' + repository + '.git',
-          credentialsId: credentialsId
-    ]]])
-  }
-}
-
 void buildGoBinary(String directory, String outputBinary, String packages) {
   dir(directory) {
     sh 'go mod vendor'
@@ -28,7 +14,7 @@ void call(Map additionalConfig) {
     branchVegaCapsule: 'main',
     branchVega: 'develop',
     branchDataNode: 'develop',
-    branchSystemTests: 'main',
+    branchSystemTests: 'develop',
     branchVegawallet: 'develop',
     branchProtos: 'develop',
     branchVegatools: 'develop',
@@ -39,7 +25,6 @@ void call(Map additionalConfig) {
     systemTestsDebug: '',
 
     preapareSteps: {},
-    nodeIdentifier: 'system-tests-capsule',
     gitCredentialsId: 'vega-ci-bot',
   ]
   
@@ -47,131 +32,130 @@ void call(Map additionalConfig) {
 
   print "config: " + config
 
-  node(config.nodeIdentifier) {
-    def testDirectoryPath
-    dir('tests') {
-      testDirectoryPath = sh(returnStdout: true, script: 'pwd').trim()
+  def testDirectoryPath
+  dir('tests') {
+    testDirectoryPath = sh(returnStdout: true, script: 'pwd').trim()
+  }
+
+  stage('prepare') {
+    cleanWs()
+    
+    config.preapareSteps()
+  }
+
+  stage('get source codes') {
+    def repositories = [ 
+      [ name: 'devops-infra', branch: config.branchDevopsInfra ],
+      [ name: 'vegacapsule', branch: config.branchVegaCapsule ],
+      [ name: 'vega', branch: config.branchVega ],
+      [ name: 'data-node', branch: config.branchDataNode ],
+      [ name: 'system-tests', branch: config.branchSystemTests ],
+      [ name: 'vegawallet', branch: config.branchVegawallet ],
+      [ name: 'protos', branch: config.branchProtos ],
+      [ name: 'vegatools', branch: config.branchVegatools ],
+    ]
+
+    parallel repositories.collectEntries{value -> [value.name, { 
+      gitClone([
+        url: 'git@github.com:vegaprotocol/' + value.name + '.git',
+        branch: value.branch,
+        directory: value.name,
+        credentialsId: config.gitCredentialsId
+      ])
+    }]}
+  }
+
+  stage('build binaries') {
+    def binaries = [
+      [ repository: 'vegacapsule', name: 'vegacapsule', packages: './main.go' ],
+      [ repository: 'vega', name: 'vega', packages: './cmd/vega/' ],
+      [ repository: 'data-node', name: 'data-node', packages: './cmd/data-node/' ],
+      [ repository: 'vegawallet', name: 'vegawallet', packages: './main.go' ],
+    ]
+    
+    parallel binaries.collectEntries{value -> [value.name, { buildGoBinary(value.repository,  testDirectoryPath + '/' + value.name, value.packages) }]}
+  }
+  
+  stage('prepare network') {
+    dir('system-tests') {
+      sh 'cp -r ./vegacapsule/multisig-setup ' + testDirectoryPath
+      sh 'cp ./vegacapsule/capsule_config.hcl ' + testDirectoryPath + '/config_system_tests.hcl'
     }
 
-    stage('prepare') {
-      cleanWs()
-      
-      config.preapareSteps()
-    }
-
-    stage('get source codes') {
-      def repositories = [ 
-        [ name: 'devops-infra', branch: config.branchDevopsInfra ],
-        [ name: 'vegacapsule', branch: config.branchVegaCapsule ],
-        [ name: 'vega', branch: config.branchVega ],
-        [ name: 'data-node', branch: config.branchDataNode ],
-        [ name: 'system-tests', branch: config.branchSystemTests ],
-        [ name: 'vegawallet', branch: config.branchVegawallet ],
-        [ name: 'protos', branch: config.branchProtos ],
-        [ name: 'vegatools', branch: config.branchVegatools ],
-      ]
-
-      parallel repositories.collectEntries{value -> [value.name, { gitClone(value.name, value.branch, value.name, config.gitCredentialsId) }]}
-    }
-
-    stage('build binaries') {
-      def binaries = [
-        [ repository: 'vegacapsule', name: 'vegacapsule', packages: './main.go' ],
-        [ repository: 'vega', name: 'vega', packages: './cmd/vega/' ],
-        [ repository: 'data-node', name: 'data-node', packages: './cmd/data-node/' ],
-        [ repository: 'vegawallet', name: 'vegawallet', packages: './main.go' ],
-      ]
-      
-      parallel binaries.collectEntries{value -> [value.name, { buildGoBinary(value.repository,  testDirectoryPath + '/' + value.name, value.packages) }]}
+    dir ('tests/multisig-setup') {
+      sh 'npm install'
     }
     
-    stage('prepare network') {
-      dir('system-tests') {
-        sh 'cp -r ./vegacapsule/multisig-setup ' + testDirectoryPath
-        sh 'cp ./vegacapsule/capsule_config.hcl ' + testDirectoryPath + '/config_system_tests.hcl'
-      }
-
-      dir ('tests/multisig-setup') {
-        sh 'npm install'
-      }
-      
-      dir ('tests') {
-          sh 'daemonize -o ' + testDirectoryPath + '/nomad.log -c ' + testDirectoryPath + ' -p ' + testDirectoryPath + '/vegacapsule_nomad.pid ' + testDirectoryPath + '/vegacapsule nomad'
-      }
-
-      dir('system-tests/scripts') {
-        sh 'make check'
-        sh 'make prepare-test-docker-image'
-        sh 'make build-test-proto'
-      }
+    dir ('tests') {
+        sh 'daemonize -o ' + testDirectoryPath + '/nomad.log -c ' + testDirectoryPath + ' -p ' + testDirectoryPath + '/vegacapsule_nomad.pid ' + testDirectoryPath + '/vegacapsule nomad'
     }
 
-    stage('start the network') {
-      dir('tests') {
-        try {
-          withCredentials([
-            usernamePassword(credentialsId: 'github-vega-ci-bot-artifacts', passwordVariable: 'TOKEN', usernameVariable:'USER')
-          ]) {
-            sh 'echo -n "' + TOKEN + '" | docker login https://ghcr.io -u "' + USER + '" --password-stdin'
-          }
-          sh './vegacapsule network bootstrap --config-path ./config_system_tests.hcl --home-path ' + testDirectoryPath + '/testnet'
-        } catch (e) {
-          throw e
-        } finally {
-          sh 'docker login https://ghcr.io'
+    dir('system-tests/scripts') {
+      sh 'make check'
+      sh 'make prepare-test-docker-image'
+      sh 'make build-test-proto'
+    }
+  }
+
+  stage('start the network') {
+    dir('tests') {
+      try {
+        withCredentials([
+          usernamePassword(credentialsId: 'github-vega-ci-bot-artifacts', passwordVariable: 'TOKEN', usernameVariable:'USER')
+        ]) {
+          sh 'echo -n "' + TOKEN + '" | docker login https://ghcr.io -u "' + USER + '" --password-stdin'
         }
-        sh './vegacapsule nodes ls-validators --home-path ' + testDirectoryPath + '/testnet > ' + testDirectoryPath + '/testnet/validators.json'
-        sh 'mkdir -p ' + testDirectoryPath + '/testnet/smartcontracts'
-        sh './vegacapsule state get-smartcontracts-addresses --home-path ' + testDirectoryPath + '/testnet > ' + testDirectoryPath + '/testnet/smartcontracts/addresses.json'
+        sh './vegacapsule network bootstrap --config-path ./config_system_tests.hcl --home-path ' + testDirectoryPath + '/testnet'
+      } catch (e) {
+        throw e
+      } finally {
+        sh 'docker login https://ghcr.io'
       }
+      sh './vegacapsule nodes ls-validators --home-path ' + testDirectoryPath + '/testnet > ' + testDirectoryPath + '/testnet/validators.json'
+      sh 'mkdir -p ' + testDirectoryPath + '/testnet/smartcontracts'
+      sh './vegacapsule state get-smartcontracts-addresses --home-path ' + testDirectoryPath + '/testnet > ' + testDirectoryPath + '/testnet/smartcontracts/addresses.json'
     }
+  }
 
-    stage('setup multisig contract') {
-      dir ('tests/multisig-setup') {
-        sh 'node main.js "' + testDirectoryPath + '/testnet/smartcontracts/addresses.json" "' + testDirectoryPath + '/testnet/validators.json"'
-      }
+  stage('setup multisig contract') {
+    dir ('tests/multisig-setup') {
+      sh 'node main.js "' + testDirectoryPath + '/testnet/smartcontracts/addresses.json" "' + testDirectoryPath + '/testnet/validators.json"'
     }
+  }
 
-    stage('run tests') {
+  stage('run tests') {
+    try {
+      dir('system-tests/scripts') {
+        withEnv([
+          'NETWORK_HOME_PATH="' + testDirectoryPath + '/testnet"',
+          'TEST_FUNCTION="' + config.systemTestsTestFunction + '"',
+          'TEST_MARK="' + config.systemTestsTestMark + '"',
+          'TEST_DIRECTORY="' + config.systemTestsTestDirectory + '"',
+          'USE_VEGACAPSULE=true',
+          'SYSTEM_TESTS_DEBUG=' + config.systemTestsDebug,
+          'VEGACAPSULE_BIN_LINUX="' + testDirectoryPath + '/vegacapsule"',
+          'SYSTEM_TESTS_LOG_OUTPUT="' + testDirectoryPath + '/log-output"'
+        ]) {
           ansiColor('xterm') {
-            try {
-              dir('system-tests/scripts') {
-                withEnv([
-                  'NETWORK_HOME_PATH="' + testDirectoryPath + '/testnet"',
-                  'TEST_FUNCTION="' + config.systemTestsTestFunction + '"',
-                  'TEST_MARK="' + config.systemTestsTestMark + '"',
-                  'TEST_DIRECTORY="' + config.systemTestsTestDirectory + '"',
-                  'USE_VEGACAPSULE=true',
-                  'SYSTEM_TESTS_DEBUG=' + config.systemTestsDebug,
-                  'VEGACAPSULE_BIN_LINUX="' + testDirectoryPath + '/vegacapsule"',
-                  'SYSTEM_TESTS_LOG_OUTPUT="' + testDirectoryPath + '/log-output"'
-                ]) {
-                  sh 'make test'
-                }
-              }
-            } catch (e) {
-              throw e
-            } finally {
-              dir('tests') {
-                archiveArtifacts artifacts: 'testnet/**/*.*',
-                          allowEmptyArchive: true
-
-                if (fileExists('log-output')) {
-                  archiveArtifacts artifacts: 'log-output/**/*.*',
-                            allowEmptyArchive: true
-                }
-              }
-              dir('system-tests') {
-                archiveArtifacts artifacts: 'build/test-reports/**/*.*',
-                          allowEmptyArchive: true
-              }
-            }
+            sh 'make test'
           }
-    }
-
-    stage('cleanup') {
+        }
+      }
+    } catch (e) {
+      throw e
+    } finally {
       dir('tests') {
-        cleanup(testDirectoryPath)
+        archiveArtifacts artifacts: 'testnet/**/*.*',
+                  allowEmptyArchive: true
+
+        if (fileExists('log-output')) {
+          archiveArtifacts artifacts: 'log-output/**/*.*',
+                    allowEmptyArchive: true
+        }
+      }
+      dir('system-tests') {
+        archiveArtifacts artifacts: 'build/test-reports/**/*.*',
+                  allowEmptyArchive: true
       }
     }
   }
@@ -180,11 +164,10 @@ void call(Map additionalConfig) {
 /**
  * Example usage
  */
-call([
-  branchSystemTests: 'vega-capsule',
-  systemTestsTestFunction: 'test_importWalletValidRecoverPhrase',
-  preapareSteps: {
-     // Move it to AMI, will be removed soon
-      sh 'sudo apt-get install -y daemonize'
-  }
-])
+// call([
+//   systemTestsTestFunction: 'test_importWalletValidRecoverPhrase',
+//   preapareSteps: {
+//     // Move it to AMI, will be removed soon
+//       sh 'sudo apt-get install -y daemonize'
+//   }
+// ])
