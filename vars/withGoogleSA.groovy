558880938eee9094b3d62898fdfd0a6fdfa87acf
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-05-23 14:33:41 +0200

    install gcloud if not in the path

--- vars/withGoogleSA.groovy
+++ vars/withGoogleSA.groovy
@@ -1,9 +1,14 @@
 def call(String credentialsId, Closure body) {
     withCredentials([file(credentialsId: credentialsId, variable: 'GC_KEY')]) {
-        // sh """
-        //     gcloud auth activate-service-account --key-file=${GC_KEY}
-        //     gcloud container clusters get-credentials gke-01 --region europe-west1 --project vegaprotocol
-        // """
+        sh """
+            if ! command -v gcloud; then
+                curl -o gcloud.tgz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-386.0.0-linux-x86_64.tar.gz
+                tar -xf gcloud.tgz
+                export PATH="\$PATH:\$PWD/google-cloud-sdk/bin"
+            fi
+            gcloud auth activate-service-account --key-file=\${GC_KEY}
+            gcloud container clusters get-credentials gke-01 --region europe-west1 --project vegaprotocol
+        """
         body()
     }
 }
