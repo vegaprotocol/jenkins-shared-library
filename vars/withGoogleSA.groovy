85c359e2b663da79c359a71ec7655081a61c3270
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-05-23 14:44:53 +0200

    support installing kubectl online

--- vars/withGoogleSA.groovy
+++ vars/withGoogleSA.groovy
@@ -6,6 +6,14 @@ def call(String credentialsId, Closure body) {
                 tar -xf gcloud.tgz
                 export PATH="\$PATH:\$PWD/google-cloud-sdk/bin"
             fi
+            if ! command -v kubectl; then
+                if [ ! -d bin ]; then
+                    mkdir bin
+                fi
+                curl -o bin/kubectl -L "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+                chmod +x bin/kubectl
+                export PATH="\$PATH:\$PWD/bin"
+            fi
             gcloud auth activate-service-account --key-file=\${GC_KEY}
             gcloud container clusters get-credentials gke-01 --region europe-west1 --project vegaprotocol
         """
