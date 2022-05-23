3fbf3289c2caa55b93209052b8dd1a6cb55a3bcc
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-05-23 14:12:14 +0200

    add auth to cluster

--- vars/withGoogleSA.groovy
+++ vars/withGoogleSA.groovy
@@ -1,6 +1,9 @@
 def call(String credentialsId, Closure body) {
     withCredentials([file(credentialsId: credentialsId, variable: 'GC_KEY')]) {
-        sh("gcloud auth activate-service-account --key-file=${GC_KEY}")
+        sh """
+            gcloud auth activate-service-account --key-file=${GC_KEY}
+            gcloud container clusters get-credentials gke-01 --region europe-west1 --project vegaprotocol
+        """
         body()
     }
 }
