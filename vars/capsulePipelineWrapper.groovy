0fdfd5e3f49d53d334918c887d69c51727c8be8d
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-07-01 13:09:48 +0200

    fix substitutions

--- vars/capsulePipelineWrapper.groovy
+++ vars/capsulePipelineWrapper.groovy
@@ -2,43 +2,43 @@ def call() {
     writeConfigs = { envName ->
         sh label: "generate templates: ${envName}", script: '''
             vegacapsule template genesis \
-                --home-path "${CONFIG_HOME}" \
-                --path "${TEMPLATES_HOME}/genesis.tmpl.json" "${FLAGS}"
+                --home-path ${CONFIG_HOME} \
+                --path ${TEMPLATES_HOME}/genesis.tmpl.json ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path "${CONFIG_HOME}" \
-                --path "${TEMPLATES_HOME}/vega.validators.tmpl.toml" \
+                --home-path ${CONFIG_HOME} \
+                --path ${TEMPLATES_HOME}/vega.validators.tmpl.toml \
                 --nodeset-group-name validators \
                 --type vega \
-                --with-merge "${FLAGS}"
+                --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
                 --home-path "${CONFIG_HOME}" \
-                --path "${TEMPLATES_HOME}/tendermint.validators.tmpl.toml" \
+                --path ${TEMPLATES_HOME}/tendermint.validators.tmpl.toml \
                 --nodeset-group-name validators \
                 --type tendermint \
-                --with-merge "${FLAGS}"
+                --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path "${CONFIG_HOME}" \
-                --path "${TEMPLATES_HOME}/vega.full.tmpl.toml" \
+                --home-path ${CONFIG_HOME} \
+                --path ${TEMPLATES_HOME}/vega.full.tmpl.toml \
                 --nodeset-group-name full \
                 --type vega \
-                --with-merge "${FLAGS}"
+                --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path "${CONFIG_HOME}" \
-                --path "${TEMPLATES_HOME}/tendermint.full.tmpl.toml" \
+                --home-path ${CONFIG_HOME} \
+                --path ${TEMPLATES_HOME}/tendermint.full.tmpl.toml \
                 --nodeset-group-name full \
                 --type tendermint \
-                --with-merge "${FLAGS}"
+                --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path "${CONFIG_HOME}" \
-                --path "${TEMPLATES_HOME}/data_node.full.tmpl.toml" \
+                --home-path ${CONFIG_HOME} \
+                --path ${TEMPLATES_HOME}/data_node.full.tmpl.toml \
                 --nodeset-group-name full \
                 --type data-node \
-                --with-merge "${FLAGS}"
+                --with-merge ${FLAGS}
             '''
     }
 
@@ -175,7 +175,7 @@ def call() {
                 stages {
                     stage('Template live config (git / networks-internal)') {
                         environment {
-                            FLAGS = "--out-dir ${env.WORKSPACE}/networks-internal/stagnet3/live-config"
+                            FLAGS = "--out-dir '${env.WORKSPACE}/networks-internal/stagnet3/live-config'"
                         }
                         steps {
                             script {
