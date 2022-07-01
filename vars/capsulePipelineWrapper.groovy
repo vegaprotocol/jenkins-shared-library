e6fe86451b1a7889ec45baf77ba6a7bcac8497f6
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-07-01 15:23:27 +0200

    fix creating PR

--- vars/capsulePipelineWrapper.groovy
+++ vars/capsulePipelineWrapper.groovy
@@ -216,7 +216,7 @@ def call() {
                                             git commit -am "Live config update"
                                             git push -u origin "\$branchName"
                                             prUrl="\$(gh pr create --title 'automated live config update' --body '${env.BUILD_URL}')"
-                                            gh pr merge --auto --delete-branch "\${prUrl}"
+                                            gh pr merge "\${prUrl}" --auto --delete-branch
                                         """
                                     }
                                 }
