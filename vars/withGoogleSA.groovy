d15637d56e1cfada60aded1f22eb3fcb4cffe08c
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-07-04 13:15:04 +0200

    speed up

--- vars/withGoogleSA.groovy
+++ vars/withGoogleSA.groovy
@@ -1,20 +1,21 @@
 def call(String credentialsId, Closure body) {
     withCredentials([file(credentialsId: credentialsId, variable: 'GC_KEY')]) {
-        // the if are used to keep pipelines stable on agents that do not have yet installed required software
+        // uncomment and put into sh block if problems with gcloud / kubectl on the building agent
+        // if ! command -v gcloud; then
+        //     curl -o gcloud.tgz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-386.0.0-linux-x86_64.tar.gz
+        //     tar -xf gcloud.tgz
+        //     export PATH="\$PATH:\$PWD/google-cloud-sdk/bin"
+        // fi
+        // if ! command -v kubectl; then
+        //     if [ ! -d bin ]; then
+        //         mkdir bin
+        //     fi
+        //     curl -o bin/kubectl -L "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+        //     chmod +x bin/kubectl
+        //     export PATH="\$PATH:\$PWD/bin"
+        // fi
+
         sh """
-            if ! command -v gcloud; then
-                curl -o gcloud.tgz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-386.0.0-linux-x86_64.tar.gz
-                tar -xf gcloud.tgz
-                export PATH="\$PATH:\$PWD/google-cloud-sdk/bin"
-            fi
-            if ! command -v kubectl; then
-                if [ ! -d bin ]; then
-                    mkdir bin
-                fi
-                curl -o bin/kubectl -L "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-                chmod +x bin/kubectl
-                export PATH="\$PATH:\$PWD/bin"
-            fi
             gcloud auth activate-service-account --key-file="\${GC_KEY}"
             gcloud container clusters get-credentials gke-01 --region europe-west1 --project vegaprotocol
         """
