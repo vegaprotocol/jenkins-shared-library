178e61ae2c0e21bad06baf900011fe964e226b8f
Daniel <daniel.1302@gmail.com>
2022-07-05 13:57:55 -0500

    feat: peer review changes

--- vars/capsulePipelineWrapper.groovy
+++ vars/capsulePipelineWrapper.groovy
@@ -2,40 +2,40 @@ def call() {
     writeConfigs = { envName ->
         sh label: "generate templates: ${envName}", script: """
             vegacapsule template genesis \
-                --home-path '""" + CONFIG_HOME + """' \
-                --path '""" + TEMPLATES_HOME + """/genesis.tmpl.json' ${FLAGS}
+                --home-path '${CONFIG_HOME}' \
+                --path '${TEMPLATES_HOME}/genesis.tmpl.json' ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path '""" + CONFIG_HOME + """' \
-                --path '""" + TEMPLATES_HOME + """/vega.validators.tmpl.toml' \
+                --home-path '${CONFIG_HOME}' \
+                --path '${TEMPLATES_HOME}/vega.validators.tmpl.toml' \
                 --nodeset-group-name validators \
                 --type vega \
                 --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path '""" + CONFIG_HOME + """' \
-                --path '""" + TEMPLATES_HOME + """/tendermint.validators.tmpl.toml' \
+                --home-path '${CONFIG_HOME}' \
+                --path '${TEMPLATES_HOME}/tendermint.validators.tmpl.toml' \
                 --nodeset-group-name validators \
                 --type tendermint \
                 --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path '""" + CONFIG_HOME + """' \
-                --path '""" + TEMPLATES_HOME + """/vega.full.tmpl.toml' \
+                --home-path '${CONFIG_HOME}' \
+                --path '${TEMPLATES_HOME}/vega.full.tmpl.toml' \
                 --nodeset-group-name full \
                 --type vega \
                 --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path '""" + CONFIG_HOME + """' \
-                --path '""" + TEMPLATES_HOME + """/tendermint.full.tmpl.toml' \
+                --home-path '${CONFIG_HOME}' \
+                --path '${TEMPLATES_HOME}/tendermint.full.tmpl.toml' \
                 --nodeset-group-name full \
                 --type tendermint \
                 --with-merge ${FLAGS}
 
             vegacapsule template node-sets \
-                --home-path '""" + CONFIG_HOME + """' \
-                --path '""" + TEMPLATES_HOME + """/data_node.full.tmpl.toml' \
+                --home-path '${CONFIG_HOME}' \
+                --path '${TEMPLATES_HOME}/data_node.full.tmpl.toml' \
                 --nodeset-group-name full \
                 --type data-node \
                 --with-merge ${FLAGS}
@@ -145,7 +145,6 @@ def call() {
                     stage('Sync remote state to local') {
                         steps {
                             sh """
-                            set -x;
                                 mkdir -p '${env.CONFIG_HOME}'
                                 aws s3 sync '${env.S3_CONFIG_HOME}/' '${env.CONFIG_HOME}/'
                             """
