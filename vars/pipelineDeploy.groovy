c0eb801eb373e4bca274ac7dde15c5b135c0226f
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-07-19 18:25:12 +0200

    #162: Add missing features to deploy pipeline

--- vars/pipelineDeploy.groovy
+++ vars/pipelineDeploy.groovy
@@ -116,6 +116,33 @@ void call() {
                             }
                         }
                     }
+                    stage('checkpoint store') {
+                        when {
+                            expression {
+                                env.NET_NAME == 'testnet'
+                            }
+                        }
+                        steps {
+                            script {
+                                doGitClone('checkpoint-store', 'main')
+                            }
+                        }
+                    }
+                    stage('vegatools') {
+                        when {
+                            expression {
+                                env.NET_NAME == 'testnet'
+                            }
+                        }
+                        steps {
+                            script {
+                                doGitClone('vegatools', 'develop')
+                            }
+                            dir('vegatools') {
+                                sh "go instal ./..."
+                            }
+                        }
+                    }
                 }
             }
             stage('Prepare'){
@@ -177,6 +204,55 @@ void call() {
                 steps {
                     script {
                         veganet('stopbots stop')
+                    }
+                }
+            }
+            stage('Backup') {
+                when {
+                    expression {
+                        env.NET_NAME == 'testnet'
+                    }
+                }
+                steps {
+                    script {
+                        veganet('chainstorecopy vegalogcopy')
+                    }
+                }
+            }
+            stage('Store checkpoint') {
+                when {
+                    expression {
+                        env.NET_NAME == 'testnet'
+                    }
+                }
+                steps {
+                    withCredentials([sshCredentials]) {
+                        script {
+                            netSsh = { command ->
+                                return 'ssh -t -i $PSSH_KEYFILE $PSSH_USER@n04.$NET_NAME.vega.xyz "sudo' + command + '"'
+                            }
+                            newestFile = sh (
+                                script: netSsh 'ls -t /home/vega/.local/state/vega/node/checkpoints/ | head -n 1',
+                                returnStdout: true
+                            ).trim()
+                            version = sh (
+                                script: netSsh "/home/vega/current/vega version | awk '{print $3}'"
+                            )
+                            sh netSsh "cp  /home/vega/.local/state/vega/node/checkpoints/${newestFile} /tmp/${newestFile}; chown `whoami`:`whoami` /tmp/${newestFile}"
+                            sh 'scp -i $PSSH_KEYFILE $PSSH_USER@n04.$NET_NAME.vega.xyz:/tmp/${newestFile} .'
+                            sh "mkdir -p checkpoint-store/Testnet-1/${version}"
+                            sh "mv ${newestFile} checkpoint-store/Testnet-1/${version}/"
+                            dir('checkpoint-store'){
+                                sh "vegatools checkpoint --file 'Testnet-1/${version}/${newestFile}' --out 'Testnet-1/${version}/${newestFile}.json'"
+                                sshagent(credentials: ['vega-ci-bot']) {
+                                    sh 'git config --global user.email "vega-ci-bot@vega.xyz"'
+                                    sh 'git config --global user.name "vega-ci-bot"'
+                                    sh "git add Testnet-1/${version}"
+                                    sh "git commit -m 'Automated update of checkpoint from ${env.BUILD_URL}'"
+                                    sh "git push"
+                                }
+                            }
+                        }
                     }
                 }
             }
