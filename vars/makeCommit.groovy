4ba99369e9c5e1079499fb21b008b61c5483b3f3
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-08-05 12:13:08 +0200

    add comment

--- vars/makeCommit.groovy
+++ vars/makeCommit.groovy
@@ -7,6 +7,7 @@ def call(args){
                     [credentialsId: creds] + args
                 )
             }
+            // use pr & automated merge (--auto flag) to pass all required CI pipelines like linters if there's any on the repository to make sure we don't make errored automated commits
             dir(args.get('directory', '.')) {
                 sh label: "sync configs to git", script: """
                     git config --global user.email "vega-ci-bot@vega.xyz"
