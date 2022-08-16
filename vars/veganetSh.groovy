6de650d19d9b58ca4351d1ed264408e1f03a6133
Daniel <daniel.1302@gmail.com>
2022-08-16 16:37:56 -0500

    feat: peer review changes

---
+++ vars/veganetSh.groovy
@@ -0,0 +1,32 @@
+void call(Map additionalConfig) {
+  Map defaultCconfig = [
+      command: '',
+      network: '',
+      directory: 'devops-infra',
+      sshCredentialsId: 'ubuntu-ansible-key',
+      dockerCredentialsId: 'github-vega-ci-bot-artifacts',
+      gcpCredentials: 'gcp-k8s',
+      timeout: 15
+  ]
+
+  Map config = defaultCconfig + additionalConfig
+  timeout(config.timeout)
+    dir(config.directory) {
+      withCredentials([sshUserPrivateKey(
+          credentialsId: config.sshCredentialsId,
+          keyFileVariable: 'PSSH_KEYFILE',
+          usernameVariable: 'PSSH_USER'
+      )]) {
+        withDockerRegistry([
+            credentialsId: config.dockerCredentialsId,
+            url: 'https://ghcr.io'
+        ]) {
+          withGoogleSA(config.gcpCredentials) {
+            sh label: 'Create markets',
+              script: './veganet.sh ' + config.network + ' ' + config.command
+          }
+        }
+      }
+    }
+  }
+}
