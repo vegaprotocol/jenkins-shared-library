2dc5d4daf46447f1a785da3af977919e70188583
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-09-23 15:11:13 +0200

    fix dsl

--- dsl/scripts/jobs.groovy
+++ dsl/scripts/jobs.groovy
@@ -173,8 +173,6 @@ vegavisorRestartNodeParams = vegavisorParamsBase << {
     choiceParam('ACTION', ['restart-node', 'quick-restart-node', 'create-node'], h('action to be performed with a node: 1. restart-node - regular restart, 2. quick-restart-node - fast restart without config updates, 3. create-node - reset node'))
     booleanParam('UNSAFE_RESET_ALL', false, 'If set to true then delete all local node state. Otherwise leave it for restart.')
     booleanParam('RANDOM_NODE', false, 'If set to true restart random node instead of the one provided in the paramaters.')
-    stringParam('DEVOPSTOOLS_BRANCH', 'main', 'Git branch, tag or hash of the vegaprotocol/devopstools repository used to select random node.')
-
 }
 
 vegavisorProtocolUpgradeParams = vegavisorParamsBase << {
