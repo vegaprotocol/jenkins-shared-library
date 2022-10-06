cd10f646d91677283e13901117a9917326e2fda2
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-10-06 16:53:45 +0200

    fix

--- vars/withDevopstools.groovy
+++ vars/withDevopstools.groovy
@@ -3,7 +3,7 @@ def call(args=[:]) {
         usernamePassword(credentialsId: 'github-vega-ci-bot-artifacts', passwordVariable: 'TOKEN', usernameVariable:'USER')
     ]) {
         dir('devopstools') {
-            sh "go run main.go ${args.command} --network + ${env.NET_NAME} --github-token ${TOKEN}"
+            sh "go run main.go ${args.command} --network ${env.NET_NAME} --github-token ${TOKEN}"
         }
     }
 }
