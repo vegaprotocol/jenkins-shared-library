3eef59dfef4de524b28f1aaf8352a9160e8af057
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-10-17 10:39:02 +0200

    Add folders to DSL

---
+++ dsl/scripts/directories.groovy
@@ -0,0 +1,55 @@
+directories = [
+    [
+        id: 'private',
+        display: '[Private]',
+    ],
+    [
+        id: 'private/Automations',
+        display: 'Automations',
+    ],
+    [
+        id :'private/Deployments',
+        display: 'Deployments',
+    ],
+    [
+        id: 'private/Deployments/devnet1',
+        display: 'Devnet 1',
+    ],
+    [
+        id: 'private/Deployments/fairground',
+        display: 'Fairground',
+    ],
+    [
+        id: 'private/Deployments/stagnet1',
+        display: 'Stagnet 1',
+    ],
+    [
+        id: 'private/Deployments/Vegacapsule',
+        display: 'Vegacapsule',
+    ],
+    [
+        id: 'private/Deployments/Veganet',
+        display: 'Veganet'
+    ],
+    [
+        id: 'private/playgrounds',
+        display: 'Playgrounds',
+    ],
+    [
+        id: 'private/Snapshots',
+        dispaly: 'Snapshots',
+    ],
+    [
+        id: 'common',
+        display: 'Common',
+    ]
+]
+
+directories.each { directory ->
+    folder(directory.id){
+        displayName(directory.display)
+        if (directory.description) {
+            description(directory.description)
+        }
+    }
+}
