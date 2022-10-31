ef62fadd721bea491e8ea00dd67543b635d20769
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-10-31 11:43:15 +0100

    fix devopstools command

--- vars/withDevopstools.groovy
+++ vars/withDevopstools.groovy
@@ -4,7 +4,7 @@ def call(args=[:]) {
     ]) {
         dir('devopstools') {
             return sh (
-                script: "go run main.go ${args.command} --network ${env.NET_NAME} --github-token \${TOKEN}",
+                script: "go run main.go --network '${env.NET_NAME}' --github-token '\${TOKEN}' ${args.command}",
                 returnStdout: args.returnStdout ?: false,
             )
         }
