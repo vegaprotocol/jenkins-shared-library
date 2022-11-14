232506b43904f578519628bc3423ce3bfc2ecf57
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-11-14 11:08:32 +0100

    fix: generating parallels for soaks

--- vars/pipelineSnapshotSoakTest.groovy
+++ vars/pipelineSnapshotSoakTest.groovy
@@ -1,5 +1,4 @@
 def call() {
-    DIRS = []
     pipeline {
         agent {
             label 'system-tests-capsule'
@@ -36,25 +35,19 @@ def call() {
                             script: "find artifacts -type d -wholename '*testnet'",
                             returnStdout: true,
                         ).trim().split("\n").findAll{ it }
-                        DIRS = DIRS.collectEntries{[basePath: basePath.split('/').find { it.startsWith('system-tests-') }]}
-                        echo "${DIRS}"
+                        DIRS = DIRS.collectEntries{ basePath -> [
+                            // generate suit names out of collected paths
+                            (basePath): basePath.split('/').find { it.startsWith('system-tests-') }
+                        ]}
                         parallel DIRS.collectEntries{ basePath, suit -> [
                             (suit): {
-                                echo "${basePath}"
+                                // generate all of the snapshots by replaying the whole chain
+                                sh "./pv-snapshot-all --tm-home='${basePath}/tendermint/${params.NODE_NAME}' --vega-home=${basePath}/vega/${params.NODE_NAME} --replay"
+                                // now load from all of the snapshots
+                                sh "./pv-snapshot-all --tm-home='${basePath}/tendermint/${params.NODE_NAME}' --vega-home=${basePath}/vega/${params.NODE_NAME}"
                             }
                         ]}
                     }
-                    // parallel DIRS.collectEntries{ basePath, suit ->
-                    //     [
-                    //         // use name of suit as name of the stage
-                    //         (suit) : {
-                    //             // generate all of the snapshots by replaying the whole chain
-                    //             sh "./pv-snapshot-all --tm-home='${basePath}/tendermint/${params.NODE_NAME}' --vega-home=${basePath}/vega/${params.NODE_NAME} --replay"
-                    //             // now load from all of the snapshots
-                    //             sh "./pv-snapshot-all --tm-home='${basePath}/tendermint/${params.NODE_NAME}' --vega-home=${basePath}/vega/${params.NODE_NAME}"
-                    //         }
-                    //     ]
-                    // }
                 }
             }
         }
