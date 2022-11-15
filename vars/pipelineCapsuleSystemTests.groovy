73f989909c27d3e56e41aaedef5d08a00196821c
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-11-15 17:29:49 +0100

    feat: try to trigger soaks

--- vars/pipelineCapsuleSystemTests.groovy
+++ vars/pipelineCapsuleSystemTests.groovy
@@ -153,15 +153,17 @@ void call() {
     }
     post {
       always {
-        script {
-          if (params.SCENARIO == 'NIGHTLY') {
-            build (
-              job: 'common/snapshot-soak-tests',
-              parameters: [
-                string(name: 'SYSTEM_TEST_JOB_NAME', value: 'common/system-tests-nightly'),
-                string(name: 'SYSTEM_TEST_BUILD_NUMBER', value: "${env.BUILD_NUMBER}" as String),
-              ]
-            )
+        node {
+          script {
+            if (params.SCENARIO == 'NIGHTLY') {
+              build (
+                job: 'common/snapshot-soak-tests',
+                parameters: [
+                  string(name: 'SYSTEM_TEST_JOB_NAME', value: 'common/system-tests-nightly'),
+                  string(name: 'SYSTEM_TEST_BUILD_NUMBER', value: "${env.BUILD_NUMBER}" as String),
+                ]
+              )
+            }
           }
         }
         cleanWs()
