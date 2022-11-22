9b6d4c0a4899d10f61944e835f9dd35c592e1f5b
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2022-11-22 14:31:58 +0100

    feat: extract common code to the seprated step & fix accessing gh cli

--- vars/pipelineVegavisorManageNode.groovy
+++ vars/pipelineVegavisorManageNode.groovy
@@ -56,30 +56,8 @@ void call() {
                     echo "params=${params.inspect()}"
                     script {
                         currentBuild.description = "action: ${params.ACTION}"
-                        if (params.RELEASE_VERSION) {
-                            RELEASE_VERSION = params.RELEASE_VERSION
-                        }
-                        if (RELEASE_VERSION == 'latest') {
-                            if (env.NET_NAME == 'testnet') {
-                                error "Do not deploy latest on testne, instead of that provide manually RELEASE_VERSION from https://github.com/vegaprotocol/vega/releases"
-                            }
-                            echo 'Using latest version for RELEASE_VERSION'
-                            // change to param if needed for other envs
-                            def RELEASE_REPO = 'vegaprotocol/vega-dev-releases'
-                            withGHCLI {
-                                RELEASE_VERSION = sh(
-                                    script: "gh release list --repo ${RELEASE_REPO} --limit 1 | awk '{print \$1}'",
-                                    returnStdout: true
-                                ).trim()
-                            }
-                            if (!RELEASE_VERSION) {
-                                error "Couldn't fetch release version, stopping pipeline!"
-                            }
-                        }
-                        if (RELEASE_VERSION) {
-                            currentBuild.description += ", release version: ${RELEASE_VERSION}"
-                        }
                     }
+                    vegavisorConfigureReleaseVersion()
                 }
             }
             stage('Checkout') {
--- vars/pipelineVegavisorProtocolUpgradeNetwork.groovy
+++ vars/pipelineVegavisorProtocolUpgradeNetwork.groovy
@@ -59,36 +59,7 @@ void call() {
                 steps {
                     sh "printenv"
                     echo "params=${params.inspect()}"
-                    script {
-                        DOCKER_VERSION = params.DOCKER_VERSION
-                        if (params.RELEASE_VERSION) {
-                            RELEASE_VERSION = params.RELEASE_VERSION
-                        }
-                        if (RELEASE_VERSION == 'latest') {
-                            if (env.NET_NAME == 'testnet') {
-                                error "Do not deploy latest on testnet, instead of that provide manually RELEASE_VERSION from https://github.com/vegaprotocol/vega/releases"
-                            }
-                            echo 'Using latest version for RELEASE_VERSION'
-                            // change to param if needed for other envs
-                            def RELEASE_REPO = 'vegaprotocol/vega-dev-releases'
-                            withGHCLI {
-                                RELEASE_VERSION = sh(
-                                    script: "gh release list --repo ${RELEASE_REPO} --limit 1 | awk '{print \$1}'",
-                                    returnStdout: true
-                                ).trim()
-                            }
-                            if (!RELEASE_VERSION) {
-                                error "Couldn't fetch release version, stopping pipeline!"
-                            }
-                            // use commit hash from release to set correct DOCKER_VERSION
-                            if (!params.DOCKER_VERSION) {
-                                DOCKER_VERSION = RELEASE_VERSION.split('-').last()
-                            }
-                        }
-                        if (RELEASE_VERSION) {
-                            currentBuild.description = "release version: ${RELEASE_VERSION}"
-                        }
-                    }
+                    vegavisorConfigureReleaseVersion()
                 }
             }
             stage('Checkout') {
