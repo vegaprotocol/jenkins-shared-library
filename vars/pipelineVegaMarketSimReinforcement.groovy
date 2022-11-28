0f1925080fe4cd15b3af90a724e3fa5b4113f614
Tom McLean <tom@vegaprotocol.io>
2022-11-28 13:06:07 +0000

    feat: Adding groovy file for Vega Market Sim RL testing run

---
+++ vars/pipelineVegaMarketSimReinforcement.groovy
@@ -0,0 +1,117 @@
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+void call() {
+    if (currentBuild.upstreamBuilds) {
+        RunWrapper upBuild = currentBuild.upstreamBuilds[0]
+        currentBuild.displayName = "#${currentBuild.id} - ${upBuild.fullProjectName} #${upBuild.id}"
+    }
+    pipeline {
+        agent any
+        options {
+            skipDefaultCheckout()
+            timeout(time: params.TIMEOUT, unit: 'MINUTES')
+            timestamps()
+            ansiColor('xterm')
+        }
+        environment {
+            CGO_ENABLED = 0
+            GO111MODULE = 'on'
+            DOCKER_IMAGE_NAME_LOCAL = 'vega_sim_learning:latest'
+        }
+        stages {
+            stage('CI Config') {
+                steps {
+                    sh "printenv"
+                    echo "params=${params.inspect()}"
+                }
+            }
+            stage('Clone vega-market-sim'){
+                options { retry(3) }
+                steps {
+                    checkout(
+                        [$class: 'GitSCM', branches: [[name: "${params.VEGA_MARKET_SIM_BRANCH}" ]],
+                        userRemoteConfigs: [[credentialsId: 'vega-ci-bot', url: 'git@github.com:vegaprotocol/vega-market-sim.git']]]
+                    )
+                }
+            }
+            stage('Clone vega'){
+                options { retry(3) }
+                steps {
+                    dir('extern/vega') {
+                        checkout(
+                            [$class: 'GitSCM', branches: [[name: "${params.VEGA_VERSION}" ]],
+                            userRemoteConfigs: [[credentialsId: 'vega-ci-bot', url: "git@github.com:${params.ORIGIN_REPO}.git"]]]
+                        )
+                    }
+                }
+            }
+            stage('Build Docker Image') {
+                options { retry(3) }
+                steps {
+                    sh label: 'Build docker image', script: '''
+                        scripts/build-docker-learning.sh
+                    '''
+                }
+            }
+            stage('Tests') {
+                parallel {
+                    stage('Learning Tests') {
+                        steps {
+                            sh label: 'Run Reinforcement Learning Test', script: '''
+                                scripts/run-docker-learning.sh ${BUILD_NUMBER}
+                            '''
+                        }
+                    }
+                }
+                post {
+                    always {
+                        archiveArtifacts artifacts: 'test_logs/**/replay'
+                    }
+                }
+            }
+        } // end: stages
+        post {
+            always {
+                sendSlackMessage()
+                retry(3) {
+                    cleanWs()
+                }
+            }
+        }
+    } // end: pipeline
+} // end: call
+
+
+void sendSlackMessage() {
+    String slackChannel = '#vega-market-sim-notify'
+    String jobURL = env.RUN_DISPLAY_URL
+    String jobName = currentBuild.id
+
+    String currentResult = currentBuild.result ?: currentBuild.currentResult
+    String duration = currentBuild.durationString - ' and counting'
+    String msg = ''
+    String color = ''
+
+    if (currentResult == 'SUCCESS') {
+        msg = ":large_green_circle: Vega Market Sim <${jobURL}|${jobName}>"
+        color = 'good'
+    } else if (currentResult == 'ABORTED') {
+        msg = ":black_circle: Vega Market Sim aborted <${jobURL}|${jobName}>"
+        color = '#000000'
+    } else {
+        msg = ":red_circle: Vega Market Sim <${jobURL}|${jobName}>"
+        color = 'danger'
+    }
+
+    if (currentBuild.upstreamBuilds) {
+        RunWrapper upBuild = currentBuild.upstreamBuilds[0]
+        msg += " for <${upBuild.absoluteUrl}|${upBuild.fullProjectName} #${upBuild.id}>"
+    }
+
+    msg += " (${duration})"
+
+    slackSend(
+        channel: slackChannel,
+        color: color,
+        message: msg,
+    )
+}
