a9698ed74220916f578ac0a9296e6ba25367c26e
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-01-03 13:06:01 +0100

    feat: fix detecting OOMs

--- vars/pipelineSnapshotTesting.groovy
+++ vars/pipelineSnapshotTesting.groovy
@@ -410,10 +410,8 @@ void call(Map config=[:]) {
                 stage('System logs') {
                     sh (
                         script: '''
-                            journalctl --list-boots | \
-                                awk '{ print $1 }' | \
-                                xargs -I{} journalctl --utc --no-pager -b {} -kqg 'killed process' -o verbose --output-fields=MESSAGE
-                            ''',
+                            journalctl | grep -i 'killed process' -C 20
+                        ''',
                         label: 'detect OOMs'
                     )
                 }
