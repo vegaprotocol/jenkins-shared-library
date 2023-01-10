5aac568a6b06c4b2d5b85133ea1f3ecad5532bbb
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-01-10 15:19:19 +0100

    Revert "feat: adjust return stdout"
    This reverts commit b4aaa6b1c51a0491dbdc40afc2f74efd05bc52b1.

--- vars/withDevopstools.groovy
+++ vars/withDevopstools.groovy
@@ -4,21 +4,10 @@ def call(args=[:]) {
         usernamePassword(credentialsId: 'github-vega-ci-bot-artifacts', passwordVariable: 'TOKEN', usernameVariable:'USER')
     ]) {
         dir('devopstools') {
-            if (args.returnStdout) {
-                sh (
-                    script: "go run main.go ${args.command} --network ${networkname} --github-token \${TOKEN} > stdout.txt",
-                )
-                def stdout = readFile(
-                    file: 'stdout.txt'
-                )
-                echo "${stdout}"
-                return stdout
-            }
-            else {
-                return sh (
-                    script: "go run main.go ${args.command} --network ${networkname} --github-token \${TOKEN}",
-                )
-            }
+            return sh (
+                script: "go run main.go ${args.command} --network ${networkname} --github-token \${TOKEN}",
+                returnStdout: args.returnStdout ?: false,
+            )
         }
     }
 }
