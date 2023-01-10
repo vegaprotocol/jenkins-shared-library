b4aaa6b1c51a0491dbdc40afc2f74efd05bc52b1
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-01-10 15:07:50 +0100

    feat: adjust return stdout

--- vars/withDevopstools.groovy
+++ vars/withDevopstools.groovy
@@ -4,10 +4,21 @@ def call(args=[:]) {
         usernamePassword(credentialsId: 'github-vega-ci-bot-artifacts', passwordVariable: 'TOKEN', usernameVariable:'USER')
     ]) {
         dir('devopstools') {
-            return sh (
-                script: "go run main.go ${args.command} --network ${networkname} --github-token \${TOKEN}",
-                returnStdout: args.returnStdout ?: false,
-            )
+            if (args.returnStdout) {
+                sh (
+                    script: "go run main.go ${args.command} --network ${networkname} --github-token \${TOKEN} > stdout.txt",
+                )
+                def stdout = readFile(
+                    file: 'stdout.txt'
+                )
+                echo "${stdout}"
+                return stdout
+            }
+            else {
+                return sh (
+                    script: "go run main.go ${args.command} --network ${networkname} --github-token \${TOKEN}",
+                )
+            }
         }
     }
 }
