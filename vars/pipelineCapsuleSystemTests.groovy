f8198868fdf43c91ee5ee002ddd930ca6b8e3434
daniel1302 <daniel.1302@gmail.com>
2023-03-24 19:22:45 +0100

    feat: fix typo of param type in system-tests pass

--- vars/pipelineCapsuleSystemTests.groovy
+++ vars/pipelineCapsuleSystemTests.groovy
@@ -118,7 +118,7 @@ void call() {
                     childParams += [booleanParam(name: 'ARCHIVE_VEGA_BINARY', value: true)]
                   }
                   if (params.SCENARIO == 'NIGHTLY' && params.SYSTEM_TESTS_NETWORK_PARAM_OVERRIDES == '') {
-                    childParams += [booleanParam(name: 'SYSTEM_TESTS_NETWORK_PARAM_OVERRIDES', value: '{"network.markPriceUpdateMaximumFrequency":"5s"}')]
+                    childParams += [stringParam(name: 'SYSTEM_TESTS_NETWORK_PARAM_OVERRIDES', value: '{"network.markPriceUpdateMaximumFrequency":"5s"}')]
                   }
                   RunWrapper downstreamBuild = build(
                     job: downstreamBuildName,
