6fdc90109a03c7c90e2ca757f48347279eb430a9
daniel1302 <daniel.1302@gmail.com>
2023-04-27 13:24:32 +0100

    fix: the dsl job

--- dsl/scripts/jobs.groovy
+++ dsl/scripts/jobs.groovy
@@ -305,14 +305,13 @@ def systemTestsParamsWrapper() {
 }
 
 def lnlSystemTestsparams(Map args=[:]) {
-    return systemTestsParamsGeneric() << {
+    return systemTestsParamsGeneric(args) << {
         stringParam('SYSTEM_TESTS_TEST_FUNCTION', 'test_checkpoint_loaded', 'Run only a tests with a specified function name. This is actually a "pytest -k $SYSTEM_TESTS_TEST_FUNCTION_NAME" command-line argument, see more: https://docs.pytest.org/en/stable/usage.html')
         stringParam('SYSTEM_TESTS_TEST_MARK', '', 'Run only a tests with the specified mark(s). This is actually a "pytest -m $SYSTEM_TESTS_TEST_MARK" command-line argument, see more: https://docs.pytest.org/en/stable/usage.html')
         stringParam('SYSTEM_TESTS_TEST_DIRECTORY', 'tests/LNL', 'Run tests from files in this directory and all sub-directories')
         stringParam('TEST_EXTRA_PYTEST_ARGS', '', 'extra args passed to system tests executiom')
         stringParam('CAPSULE_CONFIG', 'capsule_config_mainnet.hcl', 'Run tests using the given vegacapsule config file')
         booleanParam('SKIP_MULTISIGN_SETUP', true, h('When true validators are not added to multisig as signers'))
-        stringParams('NODE_LABEL', args.get('NODE_LABEL', 'general'), 'node label for server, where the job is designed to run')
     }
 }
 
