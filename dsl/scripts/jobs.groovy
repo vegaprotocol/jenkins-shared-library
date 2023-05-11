b69468c6edeb9d5d59e353f0494b199095a9e8fc
fryd <fryderyk@vega.xyz>
2023-05-11 14:46:45 +0200

    chore: use vega.rocks for devnet1

--- dsl/scripts/jobs.groovy
+++ dsl/scripts/jobs.groovy
@@ -208,6 +208,17 @@ def vegavisorManageNodeParams(args=[:]) {
         "metabase02.${args.name}.vega.xyz",
     ]
 
+
+    if (args.name == "devnet1") {
+        List nodesList = (0..15).collect { "n${it.toString().padLeft( 2, '0' )}.${args.name}.vega.rocks" } + [
+            "be.${args.name}.vega.rocks",
+            "be02.${args.name}.vega.rocks",
+            "metabase00.${args.name}.vega.rocks",
+            "metabase01.${args.name}.vega.rocks",
+            "metabase02.${args.name}.vega.rocks",
+        ]
+    }
+
     if (args.sentryNodes) {
         nodesList += ((0..15).collect { nodeNumber ->
             (0..9).collect { "sn${nodeNumber.toString().padLeft( 2, '0' )}${it}.${args.name}.vega.xyz" }
