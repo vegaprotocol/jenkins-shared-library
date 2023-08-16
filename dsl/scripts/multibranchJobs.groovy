063a7cd5f8083b58ab6fcf45ec0b89b9b6f70766
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-08-16 13:52:23 +0200

    feat: typo fix

--- dsl/scripts/multibranchJobs.groovy
+++ dsl/scripts/multibranchJobs.groovy
@@ -77,7 +77,7 @@ def createCommonMultibranchPipeline(Map args){
                         }
                     }
                 }
-                stragety {
+                strategy {
                     allBranchesSame {
 
                     }
