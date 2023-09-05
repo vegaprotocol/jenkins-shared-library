6cc433f1a61900b36bb7fd1238a8626e9fbd8c40
daniel1302 <daniel.1302@gmail.com>
2023-09-05 18:18:49 +0200

    fix: typo

--- vars/pipelineVegavisorTopupBots.groovy
+++ vars/pipelineVegavisorTopupBots.groovy
@@ -97,43 +97,41 @@ def call() {
                         }
                     }
                 }
-            }
-        }
-
-        stage('Top ups research-bots') {
-            when {
-                expression {
-                    researchBots
                 }
-            }
 
-            steps {
-                script {
-                    String researchBotsURL = 'https://research-bots-' + env.NET_NAME + '.ops.vega.xyz'
-                    vegautils.waitForValidHTTPCode(researchBotsURL + '/status', 20, 5)
+            stage('Top ups research-bots') {
+                when {
+                    expression {
+                        researchBots
+                    }
+                }
 
-                    try {
-                        withDevopstools(
-                            command: 'topup traderbot --traderbots-url ' + researchBotsURL 
-                        )
-
-                        sleep 10
-                        withGoogleSA('gcp-k8s') {
-                            sh "kubectl rollout restart statefulset traderbot-app -n ${env.NET_NAME}"
-                        }
-
+                steps {
+                    script {
+                        String researchBotsURL = 'https://research-bots-' + env.NET_NAME + '.ops.vega.xyz'
                         vegautils.waitForValidHTTPCode(researchBotsURL + '/status', 20, 5)
-                    } catch(err) {
-                        print(err)
-                        currentBuild.result = 'UNSTABLE'
+
+                        try {
+                            withDevopstools(
+                                command: 'topup traderbot --traderbots-url ' + researchBotsURL 
+                            )
+
+                            sleep 10
+                            withGoogleSA('gcp-k8s') {
+                                sh "kubectl rollout restart statefulset research-bots-app -n ${env.NET_NAME}"
+                            }
+                            sleep 20
+                            vegautils.waitForValidHTTPCode(researchBotsURL + '/status', 20, 5)
+                        } catch(err) {
+                            print(err)
+                            currentBuild.result = 'UNSTABLE'
+                        }
                     }
                 }
             }
-        }
-    }
 
-    
-    
+        }
+
         post {
             always {
                 // script {
