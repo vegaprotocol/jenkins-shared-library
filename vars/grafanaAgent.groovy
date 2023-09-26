27e8f8284c77d5417d3b3a5717718e2a2c7999e9
daniel1302 <daniel.1302@gmail.com>
2023-09-26 11:47:19 +0200

    feat: add the grafana-agent config

--- vars/grafanaAgent.groovy
+++ vars/grafanaAgent.groovy
@@ -8,7 +8,7 @@ boolean supported() {
     return exitCode == 0
 }
 
-void writeEnvFiles(String file, Map<String, String> envVars=[:]) {
+void writeEnvVars(String file, Map<String, String> envVars=[:]) {
     envVars.each {k, v -> 
         sh '''sudo sed -i 's/''' + k + '''=.*//' /etc/default/grafana-agent'''
         echo '''echo "''' + k + '''=''' + v + '''" | sudo tee -a /etc/default/grafana-agent'''
@@ -20,6 +20,7 @@ void configure(String configName, Map<String, String> extraEnvs=[:]) {
         error("Grafana-agent config not found for " + configName)
     }
 
+    writeEnvVars(extraEnvs)
     writeFile (
         text: libraryResource (
             resource: configFiles[configName]
