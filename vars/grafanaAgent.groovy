06049330baf7f455fa95c26fd6386afe14fe93bb
daniel1302 <daniel.1302@gmail.com>
2023-09-26 10:36:11 +0200

    feat: add functions for grafana-agent

---
+++ vars/grafanaAgent.groovy
@@ -0,0 +1,39 @@
+Map<String, String> configFiles = [
+    "basic": "resources/grafana-agent-basic.yaml"
+]
+
+boolean supported() {
+    exitCode = (sh returnStatus: true, script: 'systemctl list-units --all | grep grafana-agent') as int
+
+    return exitCode == 0
+}
+
+void writeEnvFiles(String file, Map<String, String> envVars=[:]) {
+    envVars.each {k, v -> 
+        sh '''sudo sed -i 's/''' + k + '''=.*//' /etc/default/grafana-agent'''
+        echo '''echo "''' + k + '''=''' + v + '''" | sudo tee -a /etc/default/grafana-agent'''
+    }
+}
+
+void configure(String configName, Map<String, String> extraEnvs=[:]) {
+    if (!configName in configFiles) {
+        error("Grafana-agent config not found for " + configName)
+    }
+
+    writeFile (
+        text: libraryResource (
+            resource: configFiles[configName]
+        ),
+        file: 'grafana-agent.yaml',
+    )
+    sh 'sudo rm /etc/grafana-agent.yaml || echo "OK: grafana-agent.yaml not found"'
+    sh 'sudo cp ./grafana-agent.yaml /etc/grafana-agent.yaml';
+}
+
+void start() {
+    sh 'sudo systemctl start grafana-agent'
+}
+
+void stop() {
+    sh 'sudo systemctl stop grafana-agent'
+}
