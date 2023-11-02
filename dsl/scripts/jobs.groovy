83a77b62c997d0d3e1e3c8e80edff010528579f0
Gordsport <83510148+gordsport@users.noreply.github.com>
2023-11-02 11:37:49 +0000

    chore: point approbation at palazzo branch
    In order to make the approbation pipeline point at the cosmicelevator branch in the specs repo this PR changes the branch its looking at.
    
    This will allow us to track AC coverage on the palazzo branch during the development/testing of this .
    
    There is a periodic (manual) task to keep the palazzo branch up-to-date with master which will mean that any changes for Palazzo specs/ACs will (with a slight lag) get updated and be present in the approbation run.

--- dsl/scripts/jobs.groovy
+++ dsl/scripts/jobs.groovy
@@ -1170,7 +1170,7 @@ def approbationParams(def config=[:]) {
 
         stringParam {
             name('SPECS_BRANCH')
-            defaultValue('cosmicelevator')
+            defaultValue('palazzo')
             description('Git branch, tag or hash of the vegaprotocol/specs repository')
             trim(true)
         }
