5bdefee71632acbf6630bce69c07176a1dbccff9
daniel1302 <daniel.1302@gmail.com>
2024-08-30 10:04:47 +0200

    feat: fix code around steps into the ansible public pipeline

--- vars/pipelineAnsiblePublic.groovy
+++ vars/pipelineAnsiblePublic.groovy
@@ -1,5 +1,5 @@
 void call(Map config=[:]) {
-    if (!config.containsKey('playbookName') || config.playbookName.length < 2) {
+    if (!config.containsKey('playbookName') || config.playbookName.length() < 2) {
         error("Pass non empty config.playbookName to the pipelinePublicAnsible call")
     }
 
@@ -19,27 +19,31 @@ void call(Map config=[:]) {
             timestamps()
             timeout(time: 35, unit: 'MINUTES')
         }
-        environment {
-        }
+        // environment {
+        // }
         stages {
             stage('Pull repositories') {
                 parallel {
                     stage('ansible-testnet-automation') {
-                        gitClone(
-                            directory: 'ansible-testnet-automation',
-                            vegaUrl: 'ansible-testnet-automation',
-                            branch: ansibleTestnetAutomationBranch,
-                            extensions: [[$class: 'LocalBranch', localBranch: "**" ]]
-                        )
+                        steps {
+                            gitClone(
+                                directory: 'ansible-testnet-automation',
+                                vegaUrl: 'ansible-testnet-automation',
+                                branch: ansibleTestnetAutomationBranch,
+                                extensions: [[$class: 'LocalBranch', localBranch: "**" ]]
+                            )
+                        }
                     }
 
                     stage('networks-config-private') {
-                        gitClone(
-                            directory: 'networks-config-private',
-                            vegaUrl: 'networks-config-private',
-                            branch: networksConfigPrivateBranch,
-                            extensions: [[$class: 'LocalBranch', localBranch: "**" ]]
-                        )
+                        steps {
+                            gitClone(
+                                directory: 'networks-config-private',
+                                vegaUrl: 'networks-config-private',
+                                branch: networksConfigPrivateBranch,
+                                extensions: [[$class: 'LocalBranch', localBranch: "**" ]]
+                            )
+                        }
                     }
                 }
             }
